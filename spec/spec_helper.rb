d427c627017e162fcb70bbd2ad659a6a6e002199
Hui Wang <hwang@freewheel.tv>
2015-08-25 18:17:34 +0800

    Finish static pages

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -38,7 +38,7 @@ RSpec.configure do |config|
     # a real object. This is generally recommended, and will default to
     # `true` in RSpec 4.
     mocks.verify_partial_doubles = true
-  end
+  end  
 
 # The settings below are suggested to provide a good initial experience
 # with RSpec, but feel free to customize to your heart's content.
